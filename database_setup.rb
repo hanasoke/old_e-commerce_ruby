f5d3a25b98fecce331577a9bc81cd0eb64c65cf4
hanasoke@gmail.com <hanasoke@gmail.com>
2025-02-14 15:19:55 +0700

    update wishlists table in database_setup.rb

--- database_setup.rb
+++ database_setup.rb
@@ -172,8 +172,8 @@ DB.execute <<-SQL
     )
 SQL
 
-begin 
-    DB.execute("ALTER TABLE wishlist ADD COLUMN profile_id INTEGER;")
-rescue SQLite3::SQLException => e 
-    puts "Column 'manufacture' already exists or another error occured: #{e.message}"
-end
+# begin 
+#     DB.execute("ALTER TABLE wishlists ADD COLUMN profile_id INTEGER;")
+# rescue SQLite3::SQLException => e 
+#     puts "Column 'profile_id' already exists or another error occured: #{e.message}"
+# end
